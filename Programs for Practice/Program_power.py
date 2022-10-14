9c1a4803b6338d9c1d5547f4ae0ecac249c7d163
Kostya <109729689+KMotrenko@users.noreply.github.com>
2022-10-14 12:19:13 +0300

    Update Program_power.py

--- Programs for Practice/Program_power.py
+++ Programs for Practice/Program_power.py
@@ -2,13 +2,12 @@ from re import S
 from termcolor import colored
 
 while True:
-    num = input(colored("\nEnter any number or 'q' to Exit: ", 'magenta'))
+    power = input(colored("\nEnter any number or 'q' to Exit: ", 'magenta'))
 
-    if num == 'q':
+    if power == 'q':
             break
-    
-    print(colored(f"\nEntered number {int(num)} of meters would be {int(num)*1e+9} nanometers", 'green'))
-    print(colored(f"Entered number {int(num)} of meters would be {int(num)*1e+6} micrometers", 'green'))
-    print(colored(f"Entered number {int(num)} of meters would be {int(num)*1e+2} centimeters", 'green'))
+    for elem in range(1, 11):
+        print(f"{elem} in {power} = {elem**int(power)}")
+
 
 print(colored("\nThank you for using this program", 'cyan'))
