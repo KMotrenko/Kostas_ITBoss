b288328eb5948e9200bd2d802f6bece6993c2836
Kostya <109729689+KMotrenko@users.noreply.github.com>
2022-10-14 12:19:51 +0300

    Update Program_meters_conversion.py

--- Programs for Practice/Program_meters_conversion.py
+++ Programs for Practice/Program_meters_conversion.py
@@ -2,13 +2,15 @@ from re import S
 from termcolor import colored
 
 while True:
-    num = input(colored("\nEnter any number or 'q' to Exit: ", 'magenta'))
+    num = input(colored("\nEnter any distance in meters or 'q' to Exit: ", 'magenta'))
 
     if num == 'q':
             break
     
-    sum_ariph = int((2*1 + 1*(int(num) - 1))*int(num) / 2)
-    print(colored(f"\nFor entered last number {int(num)} of ariphmetic progression, Sum would be: {int(sum_ariph)}", 'green'))
-    
+    print(colored(f"\nEntered number {int(num)} of meters would be {int(num)*1e+9} nanometers", 'green'))
+    print(colored(f"Entered number {int(num)} of meters would be {int(num)*1e+6} micrometers", 'green'))
+    print(colored(f"Entered number {int(num)} of meters would be {int(num)*1e+2} centimeters", 'green'))
+    print(colored(f"Entered number {int(num)} of meters would be {int(num)*1e+1} decimeters", 'green'))
+    print(colored(f"Entered number {int(num)} of meters would be {int(num)*1e-3} kilometers", 'green'))
 
-print(colored("\nThank you for using this program\n", 'cyan'))
+print(colored("\nThank you for using this program", 'cyan'))
